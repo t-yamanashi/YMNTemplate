4917343696651e84d7241076cb2b5412bf4fde67
Tomoyuki Yamanashi <ae86bg5@yahoo.co.jp>
2016-04-11 17:26:15 +0900

    V0.2

--- YMNTemplate/MainForm.Designer.cs
+++ YMNTemplate/MainForm.Designer.cs
@@ -57,7 +57,7 @@
             this.label2.Location = new System.Drawing.Point(39, 38);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(67, 12);
-            this.label2.TabIndex = 1;
+            this.label2.TabIndex = 3;
             this.label2.Text = "データファイル";
             // 
             // templateTextBox
@@ -68,7 +68,7 @@
             this.templateTextBox.Location = new System.Drawing.Point(112, 6);
             this.templateTextBox.Name = "templateTextBox";
             this.templateTextBox.Size = new System.Drawing.Size(389, 19);
-            this.templateTextBox.TabIndex = 2;
+            this.templateTextBox.TabIndex = 1;
             this.templateTextBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.fileTextBox_DragDrop);
             this.templateTextBox.DragEnter += new System.Windows.Forms.DragEventHandler(this.fileTextBox_DragEnter);
             // 
@@ -80,7 +80,7 @@
             this.dataTextBox.Location = new System.Drawing.Point(112, 35);
             this.dataTextBox.Name = "dataTextBox";
             this.dataTextBox.Size = new System.Drawing.Size(389, 19);
-            this.dataTextBox.TabIndex = 3;
+            this.dataTextBox.TabIndex = 4;
             this.dataTextBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.fileTextBox_DragDrop);
             this.dataTextBox.DragEnter += new System.Windows.Forms.DragEventHandler(this.fileTextBox_DragEnter);
             // 
@@ -89,7 +89,7 @@
             this.convertButton.Location = new System.Drawing.Point(6, 92);
             this.convertButton.Name = "convertButton";
             this.convertButton.Size = new System.Drawing.Size(75, 23);
-            this.convertButton.TabIndex = 4;
+            this.convertButton.TabIndex = 9;
             this.convertButton.Text = "変換";
             this.convertButton.UseVisualStyleBackColor = true;
             this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
@@ -103,14 +103,14 @@
             this.convertTextBox.Multiline = true;
             this.convertTextBox.Name = "convertTextBox";
             this.convertTextBox.Size = new System.Drawing.Size(577, 390);
-            this.convertTextBox.TabIndex = 5;
+            this.convertTextBox.TabIndex = 11;
             // 
             // copyButton
             // 
             this.copyButton.Location = new System.Drawing.Point(87, 92);
             this.copyButton.Name = "copyButton";
             this.copyButton.Size = new System.Drawing.Size(75, 23);
-            this.copyButton.TabIndex = 6;
+            this.copyButton.TabIndex = 10;
             this.copyButton.Text = "コピー";
             this.copyButton.UseVisualStyleBackColor = true;
             this.copyButton.Click += new System.EventHandler(this.copyButton_Click);
@@ -121,7 +121,7 @@
             this.templateSelectButton.Location = new System.Drawing.Point(507, 4);
             this.templateSelectButton.Name = "templateSelectButton";
             this.templateSelectButton.Size = new System.Drawing.Size(75, 23);
-            this.templateSelectButton.TabIndex = 7;
+            this.templateSelectButton.TabIndex = 2;
             this.templateSelectButton.Text = "選択";
             this.templateSelectButton.UseVisualStyleBackColor = true;
             this.templateSelectButton.Click += new System.EventHandler(this.templateSelectButton_Click);
@@ -132,7 +132,7 @@
             this.dataSelectButton.Location = new System.Drawing.Point(507, 33);
             this.dataSelectButton.Name = "dataSelectButton";
             this.dataSelectButton.Size = new System.Drawing.Size(75, 23);
-            this.dataSelectButton.TabIndex = 8;
+            this.dataSelectButton.TabIndex = 5;
             this.dataSelectButton.Text = "選択";
             this.dataSelectButton.UseVisualStyleBackColor = true;
             this.dataSelectButton.Click += new System.EventHandler(this.dataSelectButton_Click);
@@ -144,7 +144,7 @@
             this.fileFormatComboBox.Location = new System.Drawing.Point(112, 60);
             this.fileFormatComboBox.Name = "fileFormatComboBox";
             this.fileFormatComboBox.Size = new System.Drawing.Size(142, 20);
-            this.fileFormatComboBox.TabIndex = 9;
+            this.fileFormatComboBox.TabIndex = 7;
             // 
             // label3
             // 
@@ -152,7 +152,7 @@
             this.label3.Location = new System.Drawing.Point(19, 63);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(87, 12);
-            this.label3.TabIndex = 10;
+            this.label3.TabIndex = 6;
             this.label3.Text = "読込ファイル形式";
             // 
             // errorCheckBox
@@ -161,7 +161,7 @@
             this.errorCheckBox.Location = new System.Drawing.Point(260, 62);
             this.errorCheckBox.Name = "errorCheckBox";
             this.errorCheckBox.Size = new System.Drawing.Size(179, 16);
-            this.errorCheckBox.TabIndex = 11;
+            this.errorCheckBox.TabIndex = 8;
             this.errorCheckBox.Text = "文法エラーがあるときは即時終了";
             this.errorCheckBox.UseVisualStyleBackColor = true;
             // 
---
+++ YMNTemplate/TextAnalysis.cs
@@ -0,0 +1,80 @@
+﻿/***************************************************************************************************
+* システム名: YMNWrite(テンプレートツール)
+*   クラス名: TextAnalysis
+*       役割: 構文解析
+* 　  作成者: 山梨智之
+****************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YMNTemplate
+{
+    public class TextAnalysis
+    {
+        #region ***** フィールド ***** 
+        /// <summary>
+        /// 構文
+        /// </summary>
+        private String _syntax;
+
+        #endregion
+
+        #region ***** プロパティ ***** 
+        /// <summary>
+        /// 構文
+        /// </summary>
+        public String Syntax
+        {
+            get
+            {
+                return _syntax;
+            }
+            set
+            {
+                _syntax = value;
+            }
+        }
+        #endregion
+
+        #region ***** publicメソッド ***** 
+        
+        /// <summary>
+        /// 解析実行
+        /// </summary>
+        public void Execution()
+        {
+            List<Object> objList = new List<object>();
+            bool koubunFlg = false;
+            int st = 0;
+            int kst = 0;
+            for (int i = 0; i < _syntax.Length; i++)
+            {
+                char nowChar = _syntax[i];
+                if (nowChar == '{')
+                {
+                    koubunFlg = true;
+                }
+                if (koubunFlg)
+                {
+
+                }
+
+            }
+        }
+        
+        #endregion
+
+        #region ***** privateメソッド ***** 
+
+        #endregion
+
+        #region ***** イベント ***** 
+
+        #endregion
+
+
+    }
+}
